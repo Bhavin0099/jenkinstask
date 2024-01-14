02eb9f83f789e5e41c886976f1ef49cefa8a66a6
Bhavin0099 <153531232+Bhavin0099@users.noreply.github.com>
2024-01-14 15:43:23 +0530

    Update Clone.groovy

--- vars/Clone.groovy
+++ vars/Clone.groovy
@@ -5,5 +5,5 @@
 #}
 
 def gitclone(String repo='https://github.com/opstree/spring3hibernate.git' , branch='java7'){
-             git url: ${repo} branch: ${branch} 
+             git url: ${repo}, branch: ${branch} 
 }
