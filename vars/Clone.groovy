8e2e559fed076db60cc0d28d3069184bba55d289
Bhavin0099 <153531232+Bhavin0099@users.noreply.github.com>
2024-01-14 15:42:16 +0530

    Create Clone.groovy

---
+++ vars/Clone.groovy
@@ -0,0 +1,9 @@
+#def gitClone() {
+ #   stage('Code Checkout') {
+   #     git branch: 'java7', url: 'https://github.com/opstree/spring3hibernate.git'
+  #  }
+#}
+
+def gitclone(String repo='https://github.com/opstree/spring3hibernate.git' , branch='java7'){
+             git url: ${repo} branch: ${branch} 
+}
